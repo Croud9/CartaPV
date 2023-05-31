a81b241146e4c308b44e42db61a4809cdd6cc482
Croud9 <slavokugfk@gmail.com>
2023-05-31 12:17:29 +0300

    fix style pages

--- app/javascript/style.js
+++ app/javascript/style.js
@@ -1,4 +1,29 @@
 document.addEventListener("turbo:load", function() {
+  ///////////// Multiselect field
+
+  let multiselect_block = document.querySelectorAll(".multiselect_block");
+  multiselect_block.forEach(parent => {
+      let label = parent.querySelector(".field_multiselect");
+      let select = parent.querySelector(".field_select");
+      let text = label.innerHTML;
+      select.addEventListener("change", function(element) {
+          let selectedOptions = this.selectedOptions;
+          label.innerHTML = "";
+          for (let option of selectedOptions) {
+              let button = document.createElement("button");
+              button.type = "button";
+              button.className = "btn_multiselect";
+              button.textContent = option.innerHTML;
+              button.onclick = _ => {
+                  option.selected = false;
+                  button.remove();
+                  if (!select.selectedOptions.length) label.innerHTML = text
+              };
+              label.append(button);
+          }
+      })
+  })  
+  
   if (document.getElementById("map") !== null) {
         const btn_svg_show = document.getElementById('btn-svg-area-show');
         const svg_area = document.getElementById('svg-area');
@@ -83,30 +108,3 @@ document.addEventListener("turbo:load", function() {
 
     };
 });
-
-
-///////////// Multiselect field
-
-let multiselect_block = document.querySelectorAll(".multiselect_block");
-multiselect_block.forEach(parent => {
-    let label = parent.querySelector(".field_multiselect");
-    let select = parent.querySelector(".field_select");
-    let text = label.innerHTML;
-    select.addEventListener("change", function(element) {
-        let selectedOptions = this.selectedOptions;
-        label.innerHTML = "";
-        for (let option of selectedOptions) {
-            let button = document.createElement("button");
-            button.type = "button";
-            button.className = "btn_multiselect";
-            button.textContent = option.value;
-            button.onclick = _ => {
-                option.selected = false;
-                button.remove();
-                if (!select.selectedOptions.length) label.innerHTML = text
-            };
-            label.append(button);
-        }
-    })
-})
-
