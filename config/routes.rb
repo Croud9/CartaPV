d61d1191bb3ae60206c774a603f90128a5a7800f
Croud9 <slavokugfk@gmail.com>
2023-06-05 13:07:21 +0300

    add dockerfile

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
   root "pv_maps#index"
   get "pv_maps", to: "pv_maps#index"
-  get "get_configs_by_project", to: "pv_maps#get_configs_by_project"
   get "get_config_params", to: "pv_maps#get_config_params"
-  get "get_params_for_snapshots", to: "exports#get_params_for_snapshots"
   get "get_project_params", to: "pv_maps#get_project_params"
-  post "update_files", to: "exports#update_files"
-  post "update_configuration", to: "pv_maps#update_configuration"
-  post "update_draw_area", to: "pv_maps#update_draw_area"
+  get "get_configs_by_project", to: "pv_maps#get_configs_by_project"
+  get "get_params_for_snapshots", to: "exports#get_params_for_snapshots"
   get "export_pdf", to: "exports#show"
   get "export", to: "exports#index"
+  post "update_files", to: "exports#update_files"
+  post "update_draw_area", to: "pv_maps#update_draw_area"
+  post "update_configuration", to: "pv_maps#update_configuration"
   post 'update_model/:id', to: "pv_modules#update_model", as: 'update_model'
 
   resources :pv_modules do
